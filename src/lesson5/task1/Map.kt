be38ada6599fc32518a1f9a2b56eb84fb837fe5e
Wagner <wagnerartem@yandex.ru>
2022-09-19 15:47:09 +0300

    фикс к 5

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -244,7 +244,7 @@ fun findCheapestStuff(stuff: Map<String, Pair<String, Double>>, kind: String): S
  * Например:
  *   canBuildFrom(listOf('a', 'b', 'o'), "baobab") -> true
  */
-fun canBuildFrom(chars: List<Char>, word: String): Boolean = word.toSet() == chars.toSet()
+fun canBuildFrom(chars: List<Char>, word: String): Boolean = word.toSet().intersect(chars.toSet()) == word.toSet()
 
 /**
  * Средняя (4 балла)
